e0742d9f18d04f06c28c40e30eb123587db05a85
yoshi-automation <yoshi-automation@google.com>
2020-05-13 05:22:37 -0700

    [CHANGE ME] Re-generated  to pick up changes in the API or client library generator.

--- google/cloud/container_v1beta1/gapic/cluster_manager_client.py
+++ google/cloud/container_v1beta1/gapic/cluster_manager_client.py
@@ -377,8 +377,8 @@ class ClusterManagerClient(object):
         metadata=None,
     ):
         """
-        Creates a cluster, consisting of the specified number and type of Google
-        Compute Engine instances.
+        Creates a cluster, consisting of the specified number and type of
+        Google Compute Engine instances.
 
         By default, the cluster is created in the project's `default
         network <https://cloud.google.com/compute/docs/networks-and-firewalls#networks>`__.
@@ -757,8 +757,8 @@ class ClusterManagerClient(object):
 
                 If a dict is provided, it must be of the same form as the protobuf
                 message :class:`~google.cloud.container_v1beta1.types.NodePoolAutoscaling`
-            name (str): The name (project, location, cluster, node pool) of the node pool to set
-                autoscaler settings. Specified in the format
+            name (str): The name (project, location, cluster, node pool) of the node pool to
+                set autoscaler settings. Specified in the format
                 ``projects/*/locations/*/clusters/*/nodePools/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
@@ -858,8 +858,8 @@ class ClusterManagerClient(object):
                 by the name field.
             cluster_id (str): Required. Deprecated. The name of the cluster to upgrade.
                 This field has been deprecated and replaced by the name field.
-            logging_service (str): Required. The logging service the cluster should use to write metrics.
-                Currently available options:
+            logging_service (str): Required. The logging service the cluster should use to write
+                metrics. Currently available options:
 
                 -  "logging.googleapis.com" - the Google Cloud Logging service
                 -  "none" - no metrics will be exported from the cluster
@@ -967,8 +967,9 @@ class ClusterManagerClient(object):
 
                 -  "monitoring.googleapis.com" - the Google Cloud Monitoring service
                 -  "none" - no metrics will be exported from the cluster
-            name (str): The name (project, location, cluster) of the cluster to set monitoring.
-                Specified in the format ``projects/*/locations/*/clusters/*``.
+            name (str): The name (project, location, cluster) of the cluster to set
+                monitoring. Specified in the format
+                ``projects/*/locations/*/clusters/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
                 be retried using a default configuration.
@@ -1177,8 +1178,9 @@ class ClusterManagerClient(object):
                 the cluster, depending on whether locations are being added or removed.
 
                 This list must always include the cluster's primary zone.
-            name (str): The name (project, location, cluster) of the cluster to set locations.
-                Specified in the format ``projects/*/locations/*/clusters/*``.
+            name (str): The name (project, location, cluster) of the cluster to set
+                locations. Specified in the format
+                ``projects/*/locations/*/clusters/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
                 be retried using a default configuration.
@@ -1586,9 +1588,9 @@ class ClusterManagerClient(object):
                 `zone <https://cloud.google.com/compute/docs/zones#available>`__ to
                 return operations for, or ``-`` for all zones. This field has been
                 deprecated and replaced by the parent field.
-            parent (str): The parent (project and location) where the operations will be listed.
-                Specified in the format ``projects/*/locations/*``. Location "-" matches
-                all zones and all regions.
+            parent (str): The parent (project and location) where the operations will be
+                listed. Specified in the format ``projects/*/locations/*``. Location "-"
+                matches all zones and all regions.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
                 be retried using a default configuration.
@@ -1769,8 +1771,8 @@ class ClusterManagerClient(object):
                 by the name field.
             operation_id (str): Required. Deprecated. The server-assigned ``name`` of the operation.
                 This field has been deprecated and replaced by the name field.
-            name (str): The name (project, location, operation id) of the operation to cancel.
-                Specified in the format ``projects/*/locations/*/operations/*``.
+            name (str): The name (project, location, operation id) of the operation to
+                cancel. Specified in the format ``projects/*/locations/*/operations/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
                 be retried using a default configuration.
@@ -1851,8 +1853,8 @@ class ClusterManagerClient(object):
                 `zone <https://cloud.google.com/compute/docs/zones#available>`__ to
                 return operations for. This field has been deprecated and replaced by
                 the name field.
-            name (str): The name (project and location) of the server config to get, specified
-                in the format ``projects/*/locations/*``.
+            name (str): The name (project and location) of the server config to get,
+                specified in the format ``projects/*/locations/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
                 be retried using a default configuration.
@@ -1943,8 +1945,9 @@ class ClusterManagerClient(object):
                 by the parent field.
             cluster_id (str): Required. Deprecated. The name of the cluster.
                 This field has been deprecated and replaced by the parent field.
-            parent (str): The parent (project, location, cluster id) where the node pools will be
-                listed. Specified in the format ``projects/*/locations/*/clusters/*``.
+            parent (str): The parent (project, location, cluster id) where the node pools will
+                be listed. Specified in the format
+                ``projects/*/locations/*/clusters/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
                 be retried using a default configuration.
@@ -2041,8 +2044,8 @@ class ClusterManagerClient(object):
                 This field has been deprecated and replaced by the name field.
             node_pool_id (str): Required. Deprecated. The name of the node pool.
                 This field has been deprecated and replaced by the name field.
-            name (str): The name (project, location, cluster, node pool id) of the node pool to
-                get. Specified in the format
+            name (str): The name (project, location, cluster, node pool id) of the node pool
+                to get. Specified in the format
                 ``projects/*/locations/*/clusters/*/nodePools/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
@@ -2146,8 +2149,9 @@ class ClusterManagerClient(object):
 
                 If a dict is provided, it must be of the same form as the protobuf
                 message :class:`~google.cloud.container_v1beta1.types.NodePool`
-            parent (str): The parent (project, location, cluster id) where the node pool will be
-                created. Specified in the format ``projects/*/locations/*/clusters/*``.
+            parent (str): The parent (project, location, cluster id) where the node pool will
+                be created. Specified in the format
+                ``projects/*/locations/*/clusters/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
                 be retried using a default configuration.
@@ -2248,8 +2252,8 @@ class ClusterManagerClient(object):
                 This field has been deprecated and replaced by the name field.
             node_pool_id (str): Required. Deprecated. The name of the node pool to delete.
                 This field has been deprecated and replaced by the name field.
-            name (str): The name (project, location, cluster, node pool id) of the node pool to
-                delete. Specified in the format
+            name (str): The name (project, location, cluster, node pool id) of the node pool
+                to delete. Specified in the format
                 ``projects/*/locations/*/clusters/*/nodePools/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
@@ -2351,8 +2355,8 @@ class ClusterManagerClient(object):
                 This field has been deprecated and replaced by the name field.
             node_pool_id (str): Required. Deprecated. The name of the node pool to rollback.
                 This field has been deprecated and replaced by the name field.
-            name (str): The name (project, location, cluster, node pool id) of the node poll to
-                rollback upgrade. Specified in the format
+            name (str): The name (project, location, cluster, node pool id) of the node poll
+                to rollback upgrade. Specified in the format
                 ``projects/*/locations/*/clusters/*/nodePools/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
@@ -2461,8 +2465,8 @@ class ClusterManagerClient(object):
 
                 If a dict is provided, it must be of the same form as the protobuf
                 message :class:`~google.cloud.container_v1beta1.types.NodeManagement`
-            name (str): The name (project, location, cluster, node pool id) of the node pool to
-                set management properties. Specified in the format
+            name (str): The name (project, location, cluster, node pool id) of the node pool
+                to set management properties. Specified in the format
                 ``projects/*/locations/*/clusters/*/nodePools/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
@@ -2574,8 +2578,8 @@ class ClusterManagerClient(object):
                 labels. You must always provide an up-to-date fingerprint hash when
                 updating or changing labels. Make a <code>get()</code> request to the
                 resource to get the latest fingerprint.
-            name (str): The name (project, location, cluster id) of the cluster to set labels.
-                Specified in the format ``projects/*/locations/*/clusters/*``.
+            name (str): The name (project, location, cluster id) of the cluster to set
+                labels. Specified in the format ``projects/*/locations/*/clusters/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
                 be retried using a default configuration.
@@ -2675,8 +2679,9 @@ class ClusterManagerClient(object):
             cluster_id (str): Required. Deprecated. The name of the cluster to update.
                 This field has been deprecated and replaced by the name field.
             enabled (bool): Required. Whether ABAC authorization will be enabled in the cluster.
-            name (str): The name (project, location, cluster id) of the cluster to set legacy
-                abac. Specified in the format ``projects/*/locations/*/clusters/*``.
+            name (str): The name (project, location, cluster id) of the cluster to set
+                legacy abac. Specified in the format
+                ``projects/*/locations/*/clusters/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
                 be retried using a default configuration.
@@ -2869,8 +2874,9 @@ class ClusterManagerClient(object):
                 by the name field.
             cluster_id (str): Required. Deprecated. The name of the cluster.
                 This field has been deprecated and replaced by the name field.
-            name (str): The name (project, location, cluster id) of the cluster to complete IP
-                rotation. Specified in the format ``projects/*/locations/*/clusters/*``.
+            name (str): The name (project, location, cluster id) of the cluster to complete
+                IP rotation. Specified in the format
+                ``projects/*/locations/*/clusters/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
                 be retried using a default configuration.
@@ -2971,8 +2977,8 @@ class ClusterManagerClient(object):
             node_pool_id (str): Required. Deprecated. The name of the node pool to update.
                 This field has been deprecated and replaced by the name field.
             node_count (int): Required. The desired node count for the pool.
-            name (str): The name (project, location, cluster, node pool id) of the node pool to
-                set size. Specified in the format
+            name (str): The name (project, location, cluster, node pool id) of the node pool
+                to set size. Specified in the format
                 ``projects/*/locations/*/clusters/*/nodePools/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
@@ -3271,10 +3277,10 @@ class ClusterManagerClient(object):
             ...         pass
 
         Args:
-            parent (str): Required. The parent project where subnetworks are usable. Specified in
-                the format ``projects/*``.
-            filter_ (str): Filtering currently only supports equality on the networkProjectId and
-                must be in the form: "networkProjectId=[PROJECTID]", where
+            parent (str): Required. The parent project where subnetworks are usable. Specified
+                in the format ``projects/*``.
+            filter_ (str): Filtering currently only supports equality on the networkProjectId
+                and must be in the form: "networkProjectId=[PROJECTID]", where
                 ``networkProjectId`` is the project which owns the listed subnetworks.
                 This defaults to the parent project ID.
             page_size (int): The maximum number of resources contained in the
@@ -3367,8 +3373,8 @@ class ClusterManagerClient(object):
             >>> response = client.list_locations(parent)
 
         Args:
-            parent (str): Required. Contains the name of the resource requested. Specified in the
-                format ``projects/*``.
+            parent (str): Required. Contains the name of the resource requested. Specified in
+                the format ``projects/*``.
             retry (Optional[google.api_core.retry.Retry]):  A retry object used
                 to retry requests. If ``None`` is specified, requests will
                 be retried using a default configuration.
