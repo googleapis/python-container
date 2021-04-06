4aa11901a0a1a200d22512d4e3c0e758783ef8d8
yoshi-automation <yoshi-automation@google.com>
2021-04-06 04:22:15 -0700

    chore: upgrade gapic-generator-python to 0.43.2
    PiperOrigin-RevId: 366826395
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Mon Apr 5 10:39:44 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: 2490be32f08cccc870862ea86920f58325795c89
    Source-Link: https://github.com/googleapis/googleapis/commit/2490be32f08cccc870862ea86920f58325795c89

--- google/cloud/container_v1beta1/types/cluster_service.py
+++ google/cloud/container_v1beta1/types/cluster_service.py
@@ -931,7 +931,7 @@ class IstioConfig(proto.Message):
     Attributes:
         disabled (bool):
             Whether Istio is enabled for this cluster.
-        auth (google.cloud.container_v1beta1.types.IstioConfig.IstioAuthMode):
+        auth_ (google.cloud.container_v1beta1.types.IstioConfig.IstioAuthMode):
             The specified Istio auth mode, either none,
             or mutual TLS.
     """
@@ -945,7 +945,7 @@ class IstioConfig(proto.Message):
 
     disabled = proto.Field(proto.BOOL, number=1)
 
-    auth = proto.Field(proto.ENUM, number=2, enum=IstioAuthMode,)
+    auth_ = proto.Field(proto.ENUM, number=2, enum=IstioAuthMode,)
 
 
 class CloudRunConfig(proto.Message):
