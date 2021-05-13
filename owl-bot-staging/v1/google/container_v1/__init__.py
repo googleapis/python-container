eada8bb4711530c9139d9e5fb6658d97265e6c66
Anthonios Partheniou <partheniou@google.com>
2021-05-13 08:08:48 -0400

    chore: copy files from googleapis-gen 130ce904e5d546c312943d10f48799590f9c0f66

---
+++ owl-bot-staging/v1/google/container_v1/__init__.py
@@ -0,0 +1,213 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from .services.cluster_manager import ClusterManagerClient
+from .types.cluster_service import AcceleratorConfig
+from .types.cluster_service import AddonsConfig
+from .types.cluster_service import AuthenticatorGroupsConfig
+from .types.cluster_service import AutoUpgradeOptions
+from .types.cluster_service import AutoprovisioningNodePoolDefaults
+from .types.cluster_service import BinaryAuthorization
+from .types.cluster_service import CancelOperationRequest
+from .types.cluster_service import ClientCertificateConfig
+from .types.cluster_service import CloudRunConfig
+from .types.cluster_service import Cluster
+from .types.cluster_service import ClusterAutoscaling
+from .types.cluster_service import ClusterUpdate
+from .types.cluster_service import CompleteIPRotationRequest
+from .types.cluster_service import ConfigConnectorConfig
+from .types.cluster_service import CreateClusterRequest
+from .types.cluster_service import CreateNodePoolRequest
+from .types.cluster_service import DailyMaintenanceWindow
+from .types.cluster_service import DatabaseEncryption
+from .types.cluster_service import DefaultSnatStatus
+from .types.cluster_service import DeleteClusterRequest
+from .types.cluster_service import DeleteNodePoolRequest
+from .types.cluster_service import DnsCacheConfig
+from .types.cluster_service import GetClusterRequest
+from .types.cluster_service import GetJSONWebKeysRequest
+from .types.cluster_service import GetJSONWebKeysResponse
+from .types.cluster_service import GetNodePoolRequest
+from .types.cluster_service import GetOpenIDConfigRequest
+from .types.cluster_service import GetOpenIDConfigResponse
+from .types.cluster_service import GetOperationRequest
+from .types.cluster_service import GetServerConfigRequest
+from .types.cluster_service import HorizontalPodAutoscaling
+from .types.cluster_service import HttpLoadBalancing
+from .types.cluster_service import IPAllocationPolicy
+from .types.cluster_service import IntraNodeVisibilityConfig
+from .types.cluster_service import Jwk
+from .types.cluster_service import KubernetesDashboard
+from .types.cluster_service import LegacyAbac
+from .types.cluster_service import ListClustersRequest
+from .types.cluster_service import ListClustersResponse
+from .types.cluster_service import ListNodePoolsRequest
+from .types.cluster_service import ListNodePoolsResponse
+from .types.cluster_service import ListOperationsRequest
+from .types.cluster_service import ListOperationsResponse
+from .types.cluster_service import ListUsableSubnetworksRequest
+from .types.cluster_service import ListUsableSubnetworksResponse
+from .types.cluster_service import MaintenancePolicy
+from .types.cluster_service import MaintenanceWindow
+from .types.cluster_service import MasterAuth
+from .types.cluster_service import MasterAuthorizedNetworksConfig
+from .types.cluster_service import MaxPodsConstraint
+from .types.cluster_service import NetworkConfig
+from .types.cluster_service import NetworkPolicy
+from .types.cluster_service import NetworkPolicyConfig
+from .types.cluster_service import NodeConfig
+from .types.cluster_service import NodeManagement
+from .types.cluster_service import NodePool
+from .types.cluster_service import NodePoolAutoscaling
+from .types.cluster_service import NodeTaint
+from .types.cluster_service import Operation
+from .types.cluster_service import OperationProgress
+from .types.cluster_service import PrivateClusterConfig
+from .types.cluster_service import PrivateClusterMasterGlobalAccessConfig
+from .types.cluster_service import RecurringTimeWindow
+from .types.cluster_service import ReleaseChannel
+from .types.cluster_service import ReservationAffinity
+from .types.cluster_service import ResourceLimit
+from .types.cluster_service import ResourceUsageExportConfig
+from .types.cluster_service import RollbackNodePoolUpgradeRequest
+from .types.cluster_service import SandboxConfig
+from .types.cluster_service import ServerConfig
+from .types.cluster_service import SetAddonsConfigRequest
+from .types.cluster_service import SetLabelsRequest
+from .types.cluster_service import SetLegacyAbacRequest
+from .types.cluster_service import SetLocationsRequest
+from .types.cluster_service import SetLoggingServiceRequest
+from .types.cluster_service import SetMaintenancePolicyRequest
+from .types.cluster_service import SetMasterAuthRequest
+from .types.cluster_service import SetMonitoringServiceRequest
+from .types.cluster_service import SetNetworkPolicyRequest
+from .types.cluster_service import SetNodePoolAutoscalingRequest
+from .types.cluster_service import SetNodePoolManagementRequest
+from .types.cluster_service import SetNodePoolSizeRequest
+from .types.cluster_service import ShieldedInstanceConfig
+from .types.cluster_service import ShieldedNodes
+from .types.cluster_service import StartIPRotationRequest
+from .types.cluster_service import StatusCondition
+from .types.cluster_service import TimeWindow
+from .types.cluster_service import UpdateClusterRequest
+from .types.cluster_service import UpdateMasterRequest
+from .types.cluster_service import UpdateNodePoolRequest
+from .types.cluster_service import UsableSubnetwork
+from .types.cluster_service import UsableSubnetworkSecondaryRange
+from .types.cluster_service import VerticalPodAutoscaling
+from .types.cluster_service import WorkloadIdentityConfig
+from .types.cluster_service import WorkloadMetadataConfig
+
+
+__all__ = (
+    'AcceleratorConfig',
+    'AddonsConfig',
+    'AuthenticatorGroupsConfig',
+    'AutoUpgradeOptions',
+    'AutoprovisioningNodePoolDefaults',
+    'BinaryAuthorization',
+    'CancelOperationRequest',
+    'ClientCertificateConfig',
+    'CloudRunConfig',
+    'Cluster',
+    'ClusterAutoscaling',
+    'ClusterUpdate',
+    'CompleteIPRotationRequest',
+    'ConfigConnectorConfig',
+    'CreateClusterRequest',
+    'CreateNodePoolRequest',
+    'DailyMaintenanceWindow',
+    'DatabaseEncryption',
+    'DefaultSnatStatus',
+    'DeleteClusterRequest',
+    'DeleteNodePoolRequest',
+    'DnsCacheConfig',
+    'GetClusterRequest',
+    'GetJSONWebKeysRequest',
+    'GetJSONWebKeysResponse',
+    'GetNodePoolRequest',
+    'GetOpenIDConfigRequest',
+    'GetOpenIDConfigResponse',
+    'GetOperationRequest',
+    'GetServerConfigRequest',
+    'HorizontalPodAutoscaling',
+    'HttpLoadBalancing',
+    'IPAllocationPolicy',
+    'IntraNodeVisibilityConfig',
+    'Jwk',
+    'KubernetesDashboard',
+    'LegacyAbac',
+    'ListClustersRequest',
+    'ListClustersResponse',
+    'ListNodePoolsRequest',
+    'ListNodePoolsResponse',
+    'ListOperationsRequest',
+    'ListOperationsResponse',
+    'ListUsableSubnetworksRequest',
+    'ListUsableSubnetworksResponse',
+    'MaintenancePolicy',
+    'MaintenanceWindow',
+    'MasterAuth',
+    'MasterAuthorizedNetworksConfig',
+    'MaxPodsConstraint',
+    'NetworkConfig',
+    'NetworkPolicy',
+    'NetworkPolicyConfig',
+    'NodeConfig',
+    'NodeManagement',
+    'NodePool',
+    'NodePoolAutoscaling',
+    'NodeTaint',
+    'Operation',
+    'OperationProgress',
+    'PrivateClusterConfig',
+    'PrivateClusterMasterGlobalAccessConfig',
+    'RecurringTimeWindow',
+    'ReleaseChannel',
+    'ReservationAffinity',
+    'ResourceLimit',
+    'ResourceUsageExportConfig',
+    'RollbackNodePoolUpgradeRequest',
+    'SandboxConfig',
+    'ServerConfig',
+    'SetAddonsConfigRequest',
+    'SetLabelsRequest',
+    'SetLegacyAbacRequest',
+    'SetLocationsRequest',
+    'SetLoggingServiceRequest',
+    'SetMaintenancePolicyRequest',
+    'SetMasterAuthRequest',
+    'SetMonitoringServiceRequest',
+    'SetNetworkPolicyRequest',
+    'SetNodePoolAutoscalingRequest',
+    'SetNodePoolManagementRequest',
+    'SetNodePoolSizeRequest',
+    'ShieldedInstanceConfig',
+    'ShieldedNodes',
+    'StartIPRotationRequest',
+    'StatusCondition',
+    'TimeWindow',
+    'UpdateClusterRequest',
+    'UpdateMasterRequest',
+    'UpdateNodePoolRequest',
+    'UsableSubnetwork',
+    'UsableSubnetworkSecondaryRange',
+    'VerticalPodAutoscaling',
+    'WorkloadIdentityConfig',
+    'WorkloadMetadataConfig',
+'ClusterManagerClient',
+)
