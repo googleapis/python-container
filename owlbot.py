47b4b99dbcdebe3fc3fbeca55d4a69a9fcd1e997
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2021-08-17 14:54:14 -0600

    Update owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -90,11 +90,11 @@ s.remove_staging_dirs()
 # Add templated files
 # ----------------------------------------------------------------------------
 templated_files = common.py_library(
-python.py_samples(skip_readmes=True)
     samples=False,  # set to True only if there are samples
     microgenerator=True,
     cov_level=99,
 )
+python.py_samples(skip_readmes=True)
 s.move(templated_files, excludes=[".coveragerc"])  # microgenerator has a good .coveragerc file
 
 
