6f8cddadd4b422553553ce34cafc7b9726a9a334
shabirmean <shabirmean@google.com>
2022-02-09 20:57:45 -0500

    test: add test for delete cluster

--- samples/snippets/delete_cluster_test.py
+++ samples/snippets/delete_cluster_test.py
@@ -7,7 +7,7 @@ from google.cloud import container_v1 as gke
 
 import pytest
 
-import create_cluster as gke_create
+import delete_cluster as gke_delete
 
 PROJECT_ID = os.environ["GOOGLE_CLOUD_PROJECT"]
 ZONE = "us-central1-b"
@@ -17,12 +17,29 @@ CLUSTER_NAME = f"py-container-repo-test-{uuid.uuid4().hex[:10]}"
 @pytest.fixture(autouse=True)
 def setup_and_tear_down() -> None:
 
-    # nohing to setup here
+    # create a cluster to be deleted
+    client = gke.ClusterManagerClient()
+    cluster_location = client.common_location_path(PROJECT_ID, ZONE)
+    cluster_name = f"{cluster_location}/clusters/{CLUSTER_NAME}"
+    cluster_def = {
+        "name": cluster_name,
+        "initial_node_count": 2,
+        "node_config": {"machine_type": "e2-standard-2"},
+    }
+    op = client.create_cluster({"parent": cluster_location, "cluster": cluster_def})
+    op_id = f"{cluster_location}/operations/{op.name}"
+
+    # schedule a retry to ensure the cluster is created
+    @backoff.on_predicate(backoff.expo, lambda x: x != gke.Operation.Status.DONE, max_tries=20)
+    def wait_for_create() -> gke.Operation.Status:
+        return client.get_operation({"name": op_id}).status
+
+    wait_for_create()
 
     # run the tests here
     yield
 
-    # delete the cluster
+    # delete the cluster in case the test itself failed
     client = gke.ClusterManagerClient()
     cluster_location = client.common_location_path(PROJECT_ID, ZONE)
     cluster_name = f"{cluster_location}/clusters/{CLUSTER_NAME}"
@@ -37,12 +54,12 @@ def setup_and_tear_down() -> None:
     wait_for_delete()
 
 
-def test_create_clusters(capsys: object) -> None:
-    gke_create.create_cluster(PROJECT_ID, ZONE, CLUSTER_NAME)
+def test_delete_clusters(capsys: object) -> None:
+    gke_delete.delete_cluster(PROJECT_ID, ZONE, CLUSTER_NAME)
     out, _ = capsys.readouterr()
 
     assert "Backing off " in out
-    assert "Successfully created cluster after" in out
+    assert "Successfully deleted cluster after" in out
 
     client = gke.ClusterManagerClient()
     cluster_location = client.common_location_path(PROJECT_ID, ZONE)
@@ -52,4 +69,4 @@ def test_create_clusters(capsys: object) -> None:
     for cluster in list_response.clusters:
         list_of_clusters.append(cluster.name)
 
-    assert CLUSTER_NAME in list_of_clusters
+    assert CLUSTER_NAME not in list_of_clusters
